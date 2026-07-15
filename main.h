f62399efd52f7f044eee4b4a0376401fc1125e57
AVargas84 <adrian.f.vargas@hotmail.com>
2026-07-15 23:28:09 +0000

    Add files via upload

---
+++ main.h
@@ -0,0 +1,20 @@
+/* *****************************************************
+* Name: Adrian Vargas
+* Assignment: Arrays-CPP
+* Purpose: main.h includes standard libraries required by main.cpp
+******************************************************* */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * put all your includes and directives for main here
+ * erase this comment before submitting
+ */
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "functions.h"
+
+#endif /* MAIN_H */
+
